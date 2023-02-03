9de8c98888323a50343e8fedb6c653634fefb977
arif <arif-celik56@hotmail.com>
2023-02-04 01:04:03 +0300

    removed unneeded async-await keywords

--- main.js
+++ main.js
@@ -17,32 +17,32 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/'));
 })
 
-app.post('/ileri', async (req, res) => {
-    await press(Key.Right);
+app.post('/ileri', (req, res) => {
+    press(Key.Right);
     console.log('ileri');
     res.status(200).send('ok');
 })
 
-app.post('/bosluk', async (req, res) => {
-    await press(Key.Space);
+app.post('/bosluk', (req, res) => {
+    press(Key.Space);
     console.log('bosluk');
     res.status(200).send('ok');
 })
 
-app.post('/geri', async (req, res) => {
-    await press(Key.Left);
+app.post('/geri', (req, res) => {
+    press(Key.Left);
     console.log('geri');
     res.status(200).send('ok');
 })
 
-app.post('/yukari', async (req, res) => {
-    await press(Key.Up);
+app.post('/yukari', (req, res) => {
+    press(Key.Up);
     console.log('yukarı');
     res.status(200).send('ok');
 })
 
-app.post('/asagi', async (req, res) => {
-    await press(Key.Down);
+app.post('/asagi', (req, res) => {
+    press(Key.Down);
     console.log('aşağı');
     res.status(200).send('ok');
 })
